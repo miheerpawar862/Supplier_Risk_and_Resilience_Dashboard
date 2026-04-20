7f52686a63134bb095dcd8001acc1638ffaf2f1c
miheerpawar862 <miheer.pawar@gmail.com>
2026-04-19 21:24:24 -0400

    Updating the dashboard with New Sections and Data

--- data/defaults.js
+++
@@ -1,8 +0,0 @@
-window.DEFAULTS = {
-  lt1: 1.5, lt2: 2.0, lt3: 3.0, lt4: 5.0,
-  cap1: 6, cap2: 4, cap3: 3, cap4: 2,
-  geo1: 1.5, geo2: 2.5, geo3: 3.5, geo4: 4.5,
-  wtLead: 0.30, wtCap: 0.30, wtGeo: 0.40,
-  critLow: 1, critMed: 3, critHigh: 4,
-  thrMonitor: 8, thrDual: 15, thrSafety: 20
-};
